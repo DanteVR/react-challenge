f266e19c994d942c15a30e930cbb0d401110a661
Dante <dantevilla006@gmail.com>
2021-09-29 00:29:01 -0500

    Fixed routes

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import { paths } from '@utils';
 
 import { DashboardPage } from './DashboardPage';
 
 const Routes: React.FC<any> = () => (
   <Switch>
-    <Route exact path={paths.PATH_DASHBOARD} component={DashboardPage} />
-    <Redirect from={paths.PATH_INIT} to={paths.PATH_DASHBOARD} />
+    <Route exact path={paths.PATH_INIT} component={DashboardPage} />
   </Switch>
 );
 
